f9264e1008197fa2f43433507af91575e420c5da
Rusal Shrestha <rusalo101@gmail.com>
2022-06-15 15:20:25 -0400

    setting up project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InteractiveRating from './Components/interactive-rating-component/InteractiveRating';
+
+function App() {
+  return (
+    <div>
+      <InteractiveRating />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/interactive-rating-component/InteractiveRating.js
@@ -0,0 +1,10 @@
+const InteractiveRating = () => {
+  return (
+    <div>
+      <h1>How did we do?</h1>
+      <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+    </div>
+  )
+}
+
+export default InteractiveRating;
